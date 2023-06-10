1525124692ad1ca1789352ff52ca29cdc497a5c3
limit <limt-a@glodon.com>
2023-06-10 11:58:34 +0800

    chor: 添加样式导入插件！可自动导入样式文件！✌️

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,32 @@
-import {defineConfig} from 'vite';
-import {resolve} from 'path';
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
 import vue from '@vitejs/plugin-vue';
 import dts from 'vite-plugin-dts';
+import libCss from 'vite-plugin-libcss';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 
 export default defineConfig({
-  plugins: [vue(), vueJsx(), dts({insertTypesEntry: true, copyDtsFiles: false})],
-  resolve: {dedupe: ['vue']},
+  plugins: [
+    vue(),
+    vueJsx(),
+    dts({ insertTypesEntry: true, copyDtsFiles: false }),
+    libCss()
+  ],
+  resolve: { dedupe: ['vue'] },
   build: {
+    outDir: 'lib',
     lib: {
-      name: 'vue-turn-clock',
-      entry: resolve(__dirname, 'packages/index.ts')
+      name: 'index',
+      entry: resolve(__dirname, 'packages/index.ts'),
+      formats: ['es', 'umd'],
+      fileName: format => `index.${format}.js`,
     },
     cssCodeSplit: true,
     rollupOptions: {
       external: ['vue'],
       output: {
-        globals: {vue: 'Vue'}
-      }
-    }
-  }
-})
+        globals: { vue: 'Vue' },
+      },
+    },
+  },
+});
